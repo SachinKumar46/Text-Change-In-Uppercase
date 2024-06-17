612360b78c8ccab8a86a9a74a9d40d5b1e481a2b
Sachin Rajput <sachinrajput17798@gmail.com>
2024-06-17 12:31:34 +0530

    add some files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+
 
 function App() {
+  const[text,setText]=useState("")
+
+  const handleOnChange=(e)=>{
+    setText(e.target.value);
+  }
+  const handleOnClick=()=>{
+    let newText=text.toUpperCase()
+    setText(newText);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <div className="container">
+    <div className="row">
+      <div className="col-6 m-auto my-5 border border-black rounded py-3 px-3">
+      <form action="" >
+      <label htmlFor="" className='form-label fw-bold fs-4'>Please Type we'll change text in uppercase</label>
+    <input type="search" className='form-control my-4' placeholder='Type here'  autoComplete='off' value={text} onChange={handleOnChange}/>
+    <button type='button' className='btn btn-sm btn-primary' onClick={handleOnClick}>Submit</button>
+   </form>
+      </div>
     </div>
+   </div>
   );
 }
 
