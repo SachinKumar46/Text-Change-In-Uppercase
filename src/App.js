5a40b360fcbe86f59cabb2263f9ed0a357b51d73
Sachin Rajput <sachinrajput17798@gmail.com>
2024-06-18 11:52:20 +0530

    change buttons colors in app.js file

--- src/App.js
+++ src/App.js
@@ -21,12 +21,12 @@ function App() {
   return (
     <div className="container">
       <div className="row">
-        <div className="col-6 m-auto my-5 border border-black rounded py-4 bg-black">
+        <div className="col-6 m-auto my-4 border border-black rounded py-4 bg-black">
           <form action="" >
             <label htmlFor="" className='form-label fw-bold fs-4 text-white'>Please Type we'll change text in uppercase</label>
             <textarea name="" id="" cols="30" rows="5" className='form-control my-3' value={text} onChange={handleOnChange}></textarea>
-            <button type='button' className='btn btn-sm btn btn-outline-primary' onClick={handleOnClickUppercase}>Convert To Uppercase</button>
-            <button type='button' className='btn btn-sm btn btn-outline-primary mx-3' onClick={handleOnClickLowercase}>Convert To Lowercase</button>
+            <button type='button' className='btn btn-sm btn btn-outline-light' onClick={handleOnClickUppercase}>Convert To Uppercase</button>
+            <button type='button' className='btn btn-sm btn btn-outline-light mx-3' onClick={handleOnClickLowercase}>Convert To Lowercase</button>
           </form>
           <div className="container text-white mt-4">
             <h3 className=' mt-4'>Preview</h3>
@@ -38,5 +38,4 @@ function App() {
     </div>
   );
 }
-
 export default App;
