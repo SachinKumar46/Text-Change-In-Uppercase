30220fa88726b4d4f38c2c93bfab95cc531bcbd6
Sachin Rajput <sachinrajput17798@gmail.com>
2024-06-18 11:46:43 +0530

    Add some changes in app.js file now are counting words and as well as showing preview also

--- src/App.js
+++ src/App.js
@@ -2,27 +2,40 @@ import React, { useState } from 'react'
 
 
 function App() {
-  const[text,setText]=useState("")
+  const [text, setText] = useState("")
 
-  const handleOnChange=(e)=>{
+  const handleOnChange = (e) => {
     setText(e.target.value);
   }
-  const handleOnClick=()=>{
-    let newText=text.toUpperCase()
+  const handleOnClickUppercase = () => {
+    let newText = text.toUpperCase()
     setText(newText);
   }
+
+  const handleOnClickLowercase = () => {
+    let newText = text.toLowerCase()
+    setText(newText);
+  }
+
+
   return (
-   <div className="container">
-    <div className="row">
-      <div className="col-6 m-auto my-5 border border-black rounded py-4 bg-black">
-      <form action="" >
-      <label htmlFor="" className='form-label fw-bold fs-4 text-white'>Please Type we'll change text in uppercase</label>
-    <input type="search" className='form-control my-4' placeholder='Type here'  autoComplete='off' value={text} onChange={handleOnChange}/>
-    <button type='button' className='btn btn-sm btn-primary' onClick={handleOnClick}>Submit</button>
-   </form>
+    <div className="container">
+      <div className="row">
+        <div className="col-6 m-auto my-5 border border-black rounded py-4 bg-black">
+          <form action="" >
+            <label htmlFor="" className='form-label fw-bold fs-4 text-white'>Please Type we'll change text in uppercase</label>
+            <textarea name="" id="" cols="30" rows="5" className='form-control my-3' value={text} onChange={handleOnChange}></textarea>
+            <button type='button' className='btn btn-sm btn btn-outline-primary' onClick={handleOnClickUppercase}>Convert To Uppercase</button>
+            <button type='button' className='btn btn-sm btn btn-outline-primary mx-3' onClick={handleOnClickLowercase}>Convert To Lowercase</button>
+          </form>
+          <div className="container text-white mt-4">
+            <h3 className=' mt-4'>Preview</h3>
+            <p className=''>{text}</p>
+            <p>This preview with {text.split(" ").length} Words and {text.length} Chracter</p>
+          </div>
+        </div>
       </div>
     </div>
-   </div>
   );
 }
 
